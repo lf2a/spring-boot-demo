74c5064c1f9bda021366428a6c474bdf982de03f
Luiz Filipy <luizfilipy.aa@outlook.com>
2020-09-06 12:38:08 -0300

    spring devtools

--- src/main/java/com/lf2a/springboot/demo/springbootdemo/rest/FunRestController.java
+++ src/main/java/com/lf2a/springboot/demo/springbootdemo/rest/FunRestController.java
@@ -12,4 +12,14 @@ public class FunRestController {
     public String sayHello() {
         return "Hello World! Time on server is: " + LocalDateTime.now();
     }
+
+    @GetMapping("/workout")
+    public String getDailyWorkout() {
+        return "Run a hard 5k!";
+    }
+
+    @GetMapping("/fortune")
+    public String getDailyFortune() {
+        return "Today is your luck day.";
+    }
 }
